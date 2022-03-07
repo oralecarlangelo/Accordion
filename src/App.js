d1a62192fa723ad85d852cd10b4615d761d949ed
Carl Orale <carl@klaim.ai>
2022-03-07 10:27:56 +0800

    feature: add Accordion Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Caret from "./assets/Caret";
+import datas from "./common/data";
 
 function App() {
+  const [selected, setSelected] = React.useState(null);
+
+  const toggle = (i) => {
+    if (selected === i) {
+      setSelected(null);
+    }
+
+    setSelected(i);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full min-h-[100vh] bg-gray-50 flex justify-center items-center">
+      <div>
+        {datas.map((data, i) => {
+          return (
+            <div className="max-w-4xl mb-2 overflow-auto rounded-lg bg-emerald-200">
+              <div
+                className="flex items-center justify-between p-3 rounded-lg bg-emerald-400"
+                onClick={() => toggle(i)}
+              >
+                <h1 className="text-xl font-semibold">{data.title}</h1>
+                <div
+                  className={`text-xl transition-all duration-300 ${
+                    selected === i ? "rotate-180" : "rotate-0"
+                  }`}
+                >
+                  <i class="fa-solid fa-angle-down"></i>
+                </div>
+              </div>
+              <div
+                className={`p-5 text-sm transition-all duration-500 overflow-hidden ${
+                  selected !== i ? "max-h-[0px] p-0" : "max-h-[9999px] p-5"
+                }`}
+              >
+                {data.content}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/common/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    title: "Title 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae neque quis tellus sodales malesuada eu a sapien. Fusce finibus elementum lorem ut lobortis. Quisque fermentum turpis vel purus rutrum congue. Donec vitae purus urna. Cras malesuada nisl quis convallis accumsan. Donec id interdum tortor, at suscipit ligula. Phasellus eu libero bibendum, ornare ante vel, molestie sapien. In metus magna, ultrices id tellus sed, maximus maximus magna. Nam id odio imperdiet, blandit tortor eget, elementum ligula. Morbi condimentum porta arcu, eget congue felis dapibus non. Fusce quis hendrerit tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam vehicula hendrerit nunc ac faucibus. Proin volutpat justo nec vestibulum lacinia. Mauris a ante luctus, consequat mauris quis, dignissim tellus.",
+  },
+  {
+    title: "Title 2",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae neque quis tellus sodales malesuada eu a sapien. Fusce finibus elementum lorem ut lobortis. Quisque fermentum turpis vel purus rutrum congue. Donec vitae purus urna. Cras malesuada nisl quis convallis accumsan. Donec id interdum tortor, at suscipit ligula. Phasellus eu libero bibendum, ornare ante vel, molestie sapien. In metus magna, ultrices id tellus sed, maximus maximus magna. Nam id odio imperdiet, blandit tortor eget, elementum ligula. Morbi condimentum porta arcu, eget congue felis dapibus non. Fusce quis hendrerit tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam vehicula hendrerit nunc ac faucibus. Proin volutpat justo nec vestibulum lacinia. Mauris a ante luctus, consequat mauris quis, dignissim tellus.",
+  },
+  {
+    title: "Title 3",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae neque quis tellus sodales malesuada eu a sapien. Fusce finibus elementum lorem ut lobortis. Quisque fermentum turpis vel purus rutrum congue. Donec vitae purus urna. Cras malesuada nisl quis convallis accumsan. Donec id interdum tortor, at suscipit ligula. Phasellus eu libero bibendum, ornare ante vel, molestie sapien. In metus magna, ultrices id tellus sed, maximus maximus magna. Nam id odio imperdiet, blandit tortor eget, elementum ligula. Morbi condimentum porta arcu, eget congue felis dapibus non. Fusce quis hendrerit tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam vehicula hendrerit nunc ac faucibus. Proin volutpat justo nec vestibulum lacinia. Mauris a ante luctus, consequat mauris quis, dignissim tellus. ",
+  },
+  {
+    title: "Title 4",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae neque quis tellus sodales malesuada eu a sapien. Fusce finibus elementum lorem ut lobortis. Quisque fermentum turpis vel purus rutrum congue. Donec vitae purus urna. Cras malesuada nisl quis convallis accumsan. Donec id interdum tortor, at suscipit ligula. Phasellus eu libero bibendum, ornare ante vel, molestie sapien. In metus magna, ultrices id tellus sed, maximus maximus magna. Nam id odio imperdiet, blandit tortor eget, elementum ligula. Morbi condimentum porta arcu, eget congue felis dapibus non. Fusce quis hendrerit tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam vehicula hendrerit nunc ac faucibus. Proin volutpat justo nec vestibulum lacinia. Mauris a ante luctus, consequat mauris quis, dignissim tellus. ",
+  },
+  {
+    title: "Title 5",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae neque quis tellus sodales malesuada eu a sapien. Fusce finibus elementum lorem ut lobortis. Quisque fermentum turpis vel purus rutrum congue. Donec vitae purus urna. Cras malesuada nisl quis convallis accumsan. Donec id interdum tortor, at suscipit ligula. Phasellus eu libero bibendum, ornare ante vel, molestie sapien. In metus magna, ultrices id tellus sed, maximus maximus magna. Nam id odio imperdiet, blandit tortor eget, elementum ligula. Morbi condimentum porta arcu, eget congue felis dapibus non. Fusce quis hendrerit tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam vehicula hendrerit nunc ac faucibus. Proin volutpat justo nec vestibulum lacinia. Mauris a ante luctus, consequat mauris quis, dignissim tellus. ",
+  },
+];
+
+export default data;
